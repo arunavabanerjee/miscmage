50ee24eea9a3a075fa347d5dd409f97673378ab2
arunavabanerjee <mrarunbanerjee09@gmail.com>
2018-01-17 13:15:05 +0530

    Update mage2theme1.php

--- mage2theme1.php
+++ mage2theme1.php
@@ -1,3 +1,203 @@
+
+
+<div id="menu2" class="tab-pane fade">
+<?php
+$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+$catId = 803; // Parent Category ID
+$subcategory = $objectManager->create('Magento\Catalog\Model\Category')->load($catId);
+$subcats = $subcategory->getChildrenCategories(); ?>
+<?php //get the make ?>
+<select id="level-1" name="level-1">
+<option pdata-id="0" data-id="" data-href="" value="" selected="selected"> Select... </option>    
+<?php foreach($subcats as $subcat){
+ $_category = $objectManager->create('Magento\Catalog\Model\Category')->load($subcat->getId()); ?>
+<option pdata-id="0" data-id="<?php echo $subcat->getId();?>" data-href="<?php echo $subcat->getUrl();?>" value="<?php echo $subcat->getName();?>"> <?php echo $subcat->getName();?> </option>
+<?php } ?>
+</select>
+<?php //get the model ?>
+<select id="level-2" name="level-2">
+<option pdata-id="0" data-id="" data-href="" value="" selected="selected"> Select... </option>    
+<?php foreach($subcats as $subcat){
+ $_category = $objectManager->create('Magento\Catalog\Model\Category')->load($subcat->getId()); 
+ $subsubcats = $_category->getChildrenCategories();
+ foreach($subsubcats as $subsubcat){ 
+  $_subcategory = $objectManager->create('Magento\Catalog\Model\Category')->load($subsubcat->getId()); ?>
+<option pdata-id="<?php echo $subcat->getId();?>" data-id="<?php echo $subsubcat->getId();?>" data-href="<?php echo $subsubcat->getUrl();?>" value="<?php echo $subsubcat->getName();?>"> <?php echo $subsubcat->getName();?> </option>
+<?php }} ?>
+</select>
+<?php //get the engineyear ?>
+<select id="level-3" name="level-3">
+<option pdata-id="0" data-id="" data-href="" value="" selected="selected"> Select... </option>    
+<?php foreach($subcats as $subcat){
+ $_category = $objectManager->create('Magento\Catalog\Model\Category')->load($subcat->getId()); 
+ $subsubcats = $_category->getChildrenCategories();
+ foreach($subsubcats as $subsubcat){ 
+  $_subcategory = $objectManager->create('Magento\Catalog\Model\Category')->load($subsubcat->getId()); 
+  $subsubsubcats = $_subcategory->getChildrenCategories(); ?>
+<?php foreach($subsubsubcats as $subsubsubcat){ ?>
+<option pdata-id="<?php echo $subsubcat->getId();?>" data-id="<?php echo $subsubsubcat->getId();?>" data-href="<?php echo $subsubsubcat->getUrl();?>" value="<?php echo $subsubsubcat->getName();?>"> <?php echo $subsubsubcat->getName();?> </option>
+<?php }}} ?>
+</select>
+         
+<button type="submit" class="btn btn-default sub-btn search-btn">
+    <i class="fa fa-search" aria-hidden="true"></i>Search</button>       
+                    
+<script>
+require(["jquery"], function($){
+ var optionlvl2 = ''; var optionlvl3 = '';  
+ //change values on load    
+$(document).ready(function(){ 
+ $('#level-2').attr('disabled', 'true');
+ $('#level-3').attr('disabled', 'true');
+ $('#level-2 option').each(function(){ optionlvl2 += $(this)[0].outerHTML; }); 
+ $('#level-3 option').each(function(){ optionlvl3 += $(this)[0].outerHTML; }); 
+});    
+
+//change the values on selection    
+$('#level-1').change(function(){ 
+ $('#level-2').empty().append(optionlvl2);
+ $('#level-3').empty().append(optionlvl3);
+ var idl1 = ''; 
+ idl1 = parseInt( $('#level-1 option:selected').attr('data-id') ); //console.log(idl1);
+ $('#level-2').removeAttr('disabled');
+ $('#level-2 option').each(function(){ 
+     $(this).css('display', ''); //$(this).removeAttr('selected');
+ });
+ $('#level-2 option').each(function(){ 
+   var opt = parseInt( $(this).attr('pdata-id') ); //console.log(opt);
+   //if( $(this).attr('pdata-id').indexOf(idl1) == -1 ){
+   if( (opt != 0) && (idl1 != opt) ){ 
+     //$(this).css('display', 'none');
+     $(this).remove();
+     //$(this).css('display', 'inline');
+   } else {
+      if( opt != 0){  
+        idl2 = $(this).attr('data-id'); //$(this).attr('selected','selected');
+      }  
+   }
+ });
+});
+
+$('#level-2').change(function(){ 
+ $('#level-3').empty().append(optionlvl3); 
+ var idl2 = '';
+ idl2 = parseInt( $('#level-2 option:selected').attr('data-id') ); //console.log(idl2);
+ $('#level-3').removeAttr('disabled');
+ $('#level-3 option').each(function(){ 
+     $(this).css('display', ''); //$(this).removeAttr('selected');
+ });
+ $('#level-3 option').each(function(){ 
+  var opt = parseInt( $(this).attr('pdata-id') ); //console.log(opt);
+  //if( $(this).attr('pdata-id').indexOf(idl2) == -1 ){
+  if( (opt != 0) && (idl2 != opt) ) {  
+    // $(this).css('display', 'none');
+    $(this).remove();
+    //$(this).css('display', 'inline');
+  }
+  /*else{
+    if( opt != 0){ 
+     $(this).attr('selected','selected'); 
+    }  
+  }*/
+ });
+});
+
+//submit button
+$('#menu2 .search-btn').click(function(){
+ var $l1Url =  $('#level-1 option:selected').attr('data-href');   
+ var $l2Url =  $('#level-2 option:selected').attr('data-href');    
+ var $l3Url =  $('#level-3 option:selected').attr('data-href');    
+
+ if( $l3Url != ''){ window.location.assign( $l3Url ); return; }
+ if( $l2Url != ''){ window.location.assign( $l2Url ); return; }
+ if( $l1Url != ''){ window.location.assign( $l1Url ); return; }
+});
+
+});
+</script>                
+</div>                       
+
+============================================================================
+//category filter on home page script
+
+<script>
+require(["jquery"], function($){
+ var optionlvl2 = ''; var optionlvl3 = '';  
+ //change values on load    
+$(document).ready(function(){ 
+ $('#level-2').attr('disabled', 'true');
+ $('#level-3').attr('disabled', 'true');
+ $('#level-2 option').each(function(){ optionlvl2 += $(this)[0].outerHTML; }); 
+ $('#level-3 option').each(function(){ optionlvl3 += $(this)[0].outerHTML; }); 
+});    
+
+//change the values on selection    
+$('#level-1').change(function(){ 
+ $('#level-2').empty().append(optionlvl2);
+ $('#level-3').empty().append(optionlvl3);
+ var idl1 = ''; 
+ idl1 = parseInt( $('#level-1 option:selected').attr('data-id') ); //console.log(idl1);
+ $('#level-2').removeAttr('disabled');
+ $('#level-2 option').each(function(){ 
+     $(this).css('display', ''); //$(this).removeAttr('selected');
+ });
+ $('#level-2 option').each(function(){ 
+   var opt = parseInt( $(this).attr('pdata-id') ); //console.log(opt);
+   //if( $(this).attr('pdata-id').indexOf(idl1) == -1 ){
+   if( (opt != 0) && (idl1 != opt) ){ 
+     //$(this).css('display', 'none');
+     $(this).remove();
+     //$(this).css('display', 'inline');
+   } else {
+      if( opt != 0){  
+        idl2 = $(this).attr('data-id'); //$(this).attr('selected','selected');
+      }  
+   }
+ });
+});
+
+$('#level-2').change(function(){ 
+ $('#level-3').empty().append(optionlvl3); 
+ var idl2 = '';
+ idl2 = parseInt( $('#level-2 option:selected').attr('data-id') ); //console.log(idl2);
+ $('#level-3').removeAttr('disabled');
+ $('#level-3 option').each(function(){ 
+     $(this).css('display', ''); //$(this).removeAttr('selected');
+ });
+ $('#level-3 option').each(function(){ 
+  var opt = parseInt( $(this).attr('pdata-id') ); //console.log(opt);
+  //if( $(this).attr('pdata-id').indexOf(idl2) == -1 ){
+  if( (opt != 0) && (idl2 != opt) ) {  
+    // $(this).css('display', 'none');
+    $(this).remove();
+    //$(this).css('display', 'inline');
+  }
+  /*else{
+    if( opt != 0){ 
+     $(this).attr('selected','selected'); 
+    }  
+  }*/
+ });
+});
+
+//submit button
+$('#menu2 .search-btn').click(function(){
+ var $l1Url =  $('#level-1 option:selected').attr('data-href');   
+ var $l2Url =  $('#level-2 option:selected').attr('data-href');    
+ var $l3Url =  $('#level-3 option:selected').attr('data-href');    
+
+ if( $l3Url != ''){ window.location.assign( $l3Url ); return; }
+ if( $l2Url != ''){ window.location.assign( $l2Url ); return; }
+ if( $l1Url != ''){ window.location.assign( $l1Url ); return; }
+});
+
+});
+</script>                
+
+
+
+
+===============================================================
 http://devdocs.magento.com/guides/v2.2/frontend-dev-guide/themes/theme-create.html
 ===============================================================
 Create a storefront theme: walkthrough
