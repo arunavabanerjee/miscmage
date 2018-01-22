858f871cdd264de1215549a76d21c9dde84c3448
arunavabanerjee <mrarunbanerjee09@gmail.com>
2018-01-22 17:52:02 +0530

    Update mage2-misc3.php

--- mage2-misc3.php
+++ mage2-misc3.php
@@ -1,4 +1,15 @@
 
+//static block
+<referenceContainer name="content">
+<block class="Magento\Cms\Block\Block" name="block_identifier">
+<arguments>
+<argument name="block_id" xsi:type="string">block_identifier</argument>
+</arguments>
+</block>
+</referenceContainer>
+<?php echo $block->getLayout()->createBlock('Magento\Cms\Block\Block')->setBlockId('block_identifier')->toHtml();?>
+{{block class="Magento\\Cms\\Block\\Block" block_id="block_identifier"}}
+
 //template in a static block
 {{block class="Magento\Framework\View\Element\Template" template="Vendor_Module::myfiles/myfile.phtml"}}
 {{block class="Magento\CatalogSearch\Block\Advanced\Form" name="block_name" template="Magento_CatalogSearch::advanced/form.phtml"}}
