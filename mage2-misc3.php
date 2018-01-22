10cc67214e22484bec961fb8a48393cc0942d9f9
arunavabanerjee <mrarunbanerjee09@gmail.com>
2018-01-22 17:50:07 +0530

    Update mage2-misc3.php

--- mage2-misc3.php
+++ mage2-misc3.php
@@ -1,4 +1,8 @@
 
+//template in a static block
+{{block class="Magento\Framework\View\Element\Template" template="Vendor_Module::myfiles/myfile.phtml"}}
+{{block class="Magento\CatalogSearch\Block\Advanced\Form" name="block_name" template="Magento_CatalogSearch::advanced/form.phtml"}}
+
 //current url
 $urlInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface'); 
 $urlInterface->getCurrentUrl();
