0ea02fb2b8438ced936b0a6341191f3ec9a3e6e0
arunavabanerjee <mrarunbanerjee09@gmail.com>
2019-09-16 00:07:03 +0530

    Create ps_start.php

---
+++ ps_start.php
@@ -0,0 +1,19 @@
+//---- ps variables
+$img_ps_dir       // the PrestaShop image directory
+$img_cat_dir      // current category's images directory
+$img_lang_dir     // current language's images directory
+$img_prod_dir     // current product's images directory
+$img_manu_dir     // current manufacturer's images directory
+$img_sup_dir      // current supplier's images directory
+$img_ship_dir     // current carrier's (shipping) images directory
+$img_dir          // current theme’s images directory
+$css_dir          // current theme’s CSS directory
+$js_dir           // current theme’s JavaScript directory
+$tpl_dir          // current theme’s directory (physical path on machine)
+$tpl_uri          // current theme's directory (web-root relative path)
+$modules_dir      // modules directory
+$mail_dir         // mail templates directory
+$pic_dir          // pictures upload directory
+
+
+
